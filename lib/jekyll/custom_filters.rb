0bddcf3a6741dd43415b5181c296e62413c5cfdf
sixones <sixones@me.com>
2009-04-20 22:17:12 +0100

    added invoke_filter method (incomplete), and basic hello world filter

--- lib/jekyll/custom_filters.rb
+++ lib/jekyll/custom_filters.rb
@@ -8,6 +8,18 @@ module Jekyll
       def self.included(base)
         @@extensions << base
       end
+      
+      def self.invoke_filter(name, params)
+        self.send(name, params)
+      end
+    end
+    
+    module Basic
+      include Jekyll::Filters::Custom
+      
+      def hello_world(input)
+        "Hello World!"
+      end
     end
   end
 end
